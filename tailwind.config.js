5461ac28cf70f2c20dfbadd2bcd0c25a82a6854c
unknown <dineshtandan2081@gmail.com>
2025-02-23 18:25:52 +0545

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [""], // Adjust to your project
+  theme: {
+    extend: {
+      fontFamily: {
+        fiesole: ["Fiesole", "sans-serif"], // Adding the custom font
+      },
+    },
+  },
+  plugins: [],
+};
